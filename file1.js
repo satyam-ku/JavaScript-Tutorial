fac4c561fdf41466b13b222f805d2896831b8ba9
satyam-ku <rajsatyam2005@gmail.com>
2026-02-10 22:54:06 +0530

    add introduction

---
+++ file1.js
@@ -0,0 +1,14 @@
+console.log("Hello World..!!!")
+let a = 10;
+let b = 20;
+let c = a+b;
+console.log("The sum of a and b is: " + c);
+
+//comments
+/*
+console.log("line 1");
+console.log("line 1");
+console.log("line 1");
+*/
+
+
