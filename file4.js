61cdafbeea41810f0d84655397f8604156867415
satyam-ku <rajsatyam2005@gmail.com>
2026-02-10 22:56:43 +0530

    add conditional

---
+++ file4.js
@@ -0,0 +1,11 @@
+let a = 10;
+let  b = 30;
+if(a>b){
+    console.log("a is greater than b");
+}else{
+    console.log("b is greater than a");
+}
+
+if(a>b) console.log("a is greater than b");
+else console.log("b is greater than a");
+
