c6092a751f4335b77e136517dfd602beb4effc98
satyam-ku <rajsatyam2005@gmail.com>
2026-02-11 15:37:40 +0530

    practice of arrow function

---
+++ file10.js
@@ -0,0 +1,13 @@
+// const add = (x,y) => {
+//     return x+y;
+// }
+// const result = add(4,5)
+// console.log(result)
+
+const add = (x,y) => x+y;
+const result = add(4,5)
+console.log(result)
+
+const sqr = (x) => x*x;
+const ans = sqr(5);
+console.log(ans)
