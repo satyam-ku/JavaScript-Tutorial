608448083a70a1a68d67005b7036500a220e7f49
satyam-ku <rajsatyam2005@gmail.com>
2026-02-11 15:04:55 +0530

    add datatypes and typecast

---
+++ file8.js
@@ -0,0 +1,25 @@
+// let a =10;
+// let name = "mnisa";
+// let flag  = true;
+// let rating = 4.5;
+// let score  = null;
+// let city
+// console.log(typeof a);
+// console.log(typeof name);
+// console.log(typeof flag);
+// console.log(typeof rating);
+// console.log(typeof score);
+// console.log(typeof city);
+
+let a = "10";
+console.log(typeof a);
+// let a = parseInt(a); //string to number
+a = Number(a); //string to number
+console.log(typeof a);
+
+let b = true;
+console.log(typeof b);
+let flag  = String(b); //boolean to string
+console.log(typeof flag);
+flag = Boolean(flag); //string to boolean
+console.log(typeof flag);
