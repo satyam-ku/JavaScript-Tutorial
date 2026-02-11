2ec0ccb5544dad10e52638f6898e76fbe2c5dada
satyam-ku <rajsatyam2005@gmail.com>
2026-02-11 12:35:07 +0530

    add update conditional

--- file4.js
+++ file4.js
@@ -1,11 +1,12 @@
 let a = 10;
-let  b = 30;
-if(a>b){
-    console.log("a is greater than b");
-}else{
-    console.log("b is greater than a");
-}
+let  b = 10;
+// if(a>b){
+//     console.log("a is greater than b");
+// }else{
+//     console.log("b is greater than a");
+// }
 
 if(a>b) console.log("a is greater than b");
-else console.log("b is greater than a");
+else if(b>a) console.log("b is greater than a");
+else console.log("a and b are equal");
 
