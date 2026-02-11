352bfbafc234c3cb5c3a59d83cfb4c1967aa4bc6
satyam-ku <rajsatyam2005@gmail.com>
2026-02-11 12:35:35 +0530

    add while loop

---
+++ file5.js
@@ -0,0 +1,22 @@
+let i =1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+while(i<=10){
+    i++;
+    console.log(i);
+    if(i==5){
+        break;
+    }
+}
+
+while(i<=10){
+    i++;
+    console.log(i);
+    if(i==5){
+        continue;
+    }
+}
+
