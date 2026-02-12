59f4a21281495327828e9740ed6c1197c55259b9
satyam-ku <rajsatyam2005@gmail.com>
2026-02-12 11:20:56 +0530

    add map method

---
+++ file19.js
@@ -0,0 +1,25 @@
+// const products = ["product1", "product2", "product3"];
+const products = [
+    {id:1,name:"LapTop",price: 25000,description:"this is laptop"},  
+    {id:2,name:"Mobile",price: 15000,description:"this is mobile"},
+    {id:3,name:"Tablet",price: 20000,description:"this is tablet"}
+]
+// products.forEach( (product) => {console.log(product)})
+
+// let cart = []
+// products.forEach( (product) => {
+//     product.quantity = 1
+//     product.total = product.price * product.quantity
+//     //cart.push(product)
+//     cart = [...cart,product]
+// })
+
+const cart = products.map( (product) => {
+    const obj = {
+        ...product,
+        quantity: 1,
+    }
+    return obj
+})
+console.log(cart)
+
