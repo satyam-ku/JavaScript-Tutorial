cdd642b1caef2aac9353d50653989df773443a27
satyam-ku <rajsatyam2005@gmail.com>
2026-02-16 09:43:02 +0530

    add import

---
+++ file30.js
@@ -0,0 +1,11 @@
+import {add,subtract} from "./calc.js"
+import {name} from "./calc.js"
+import {products} from "./data.js"
+const res = add(3,5)
+console.log(res)
+const ress = subtract(5,2)
+console.log(ress)
+
+console.log(name)
+
+console.log(products)
