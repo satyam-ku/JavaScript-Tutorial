9d2551b0247d31fd470f75a61978323afe703544
satyam-ku <rajsatyam2005@gmail.com>
2026-02-16 11:34:29 +0530

    add string methods

---
+++ file37.js
@@ -0,0 +1,13 @@
+let str = "hellooo"
+
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+
+console.log(str.startsWith("H"))
+console.log(str.endsWith("o"))
+
+console.log(str.includes("l"))
+console.log(str.indexOf("e"))
+
+console.log(str.substring(1,3))
+console.log(str.slice(1,3))
