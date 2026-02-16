1bcacdfd3892f42931be619e8706c6f174cb280e
satyam-ku <rajsatyam2005@gmail.com>
2026-02-16 13:24:36 +0530

    add date methods

---
+++ file38.js
@@ -0,0 +1,25 @@
+let d = new Date()
+console.log(d)
+
+//get methods
+
+console.log(d.getDate())
+console.log(d.getMonth())
+console.log(d.getFullYear())
+console.log(d.getHours())
+console.log(d.getMinutes())
+console.log(d.getSeconds())
+console.log(d.getDay())
+console.log(Date.now())
+
+
+const dd = new Date();
+console.log(dd)
+dd.setFullYear(2027)
+dd.setMonth(10)
+dd.setDate(11)
+console.log(dd)
+dd.setMinutes(10)
+dd.setSeconds(10)
+dd.setHours(10)
+console.log(dd)
