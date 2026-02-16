962fd5b4e6bf490699cb14fc002882e5da9be364
satyam-ku <rajsatyam2005@gmail.com>
2026-02-16 09:42:00 +0530

    add array operators

---
+++ file28.js
@@ -0,0 +1,5 @@
+const  names = ["Rajan", "Cathhy", "Anny", "Vivian"]
+names.forEach((name,index) => console.log(index,name))
+
+const score = [56,86,69,78]
+names.forEach((name,index) => console.log(name,score[index]))
