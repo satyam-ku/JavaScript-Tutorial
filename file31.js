a70666c7e07b7a96dc1d651fed5bb3f109ddfeba
satyam-ku <rajsatyam2005@gmail.com>
2026-02-16 09:43:19 +0530

    add export

---
+++ file31.js
@@ -0,0 +1,8 @@
+import {product} from "./products.js"
+import {compName} from "./header.js"
+import {copyright} from "./footer.js"
+console.log(compName)
+console.log("--------------------------------------")
+console.table(product)
+console.log("--------------------------------------")
+console.log(copyright)
