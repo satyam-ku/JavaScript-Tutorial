d0c99b914a38762bc307b656668d36c0029dd8ec
satyam-ku <rajsatyam2005@gmail.com>
2026-02-16 20:39:52 +0530

    add Updated files

---
+++ part3/file30.js
@@ -0,0 +1,11 @@
+import {add,subtract} from "../part4/calc.js"
+import {name} from "../part4/calc.js"
+import {products} from "../part4/data.js"
+const res = add(3,5)
+console.log(res)
+const ress = subtract(5,2)
+console.log(ress)
+
+console.log(name)
+
+console.log(products)
---
+++ part5/file40.js
@@ -0,0 +1,15 @@
+//Promises
+
+function f1(){
+    setTimeout(()=>{
+        console.log("This is f1 Function.")
+    },5000)
+}
+
+function f2(){
+    console.log("This is f2 Function.")
+}
+
+f1()
+f2()
+
