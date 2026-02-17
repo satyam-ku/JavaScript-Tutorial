5b9c5c4f104aa9a3b708df5856321e42dd5d4985
satyam-ku <rajsatyam2005@gmail.com>
2026-02-17 11:19:04 +0530

    add promises updated

--- part5/file47.js
+++ part5/file47.js
@@ -15,21 +15,27 @@ function getStudentInfo(id) {
 function getExamInfo(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve("Exam Info Resolved");
+      const found = students.find((student)=>student.id===id);
+      if(found)  resolve("Exam Info Resolved");
+      else reject("Score is too low.")
+     
     }, 3000);
   });
 }
 function getAttendanceInfo(id) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve("Attendance Info Resolved");
+      const found = students.find((student)=>student.id===id);
+      if(found)  resolve("Attendance Info Resolved");
+      else reject("Attendece too Low")
+      
     }, 1000);
   });
 }
 
 async function main() {
   try {
-    const studentId = 2;
+    const studentId = 1;
     const result = await Promise.all([
       getStudentInfo(studentId),
       getAttendanceInfo(studentId),
